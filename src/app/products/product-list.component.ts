1518042bc8139c7387f109b9a0df128da8c935cc
Anna Cunningham <ac232@hotmail.co.uk>
2021-09-14 14:42:44 +0100

    added stylesheet for component

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -4,7 +4,8 @@ import { IProduct } from "./product";
 //since we defined the HTML file in the same folder as the component we can use the ./ relative path syntax here
 @Component({
     selector: 'pm-products', //can be used as directive elsewhere
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
 
 export class ProductListComponent {
