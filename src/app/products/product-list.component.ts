a296b61d79afb06e4abd9b66a1ca29fa1a46849a
Anna Cunningham <ac232@hotmail.co.uk>
2021-09-28 15:40:52 +0100

    injecting the service

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "./products.service";
 
 //since we defined the HTML file in the same folder as the component we can use the ./ relative path syntax here
 @Component({
@@ -26,27 +27,30 @@ export class ProductListComponent implements OnInit{
 
     filteredProducts: IProduct[] =[];
     products: IProduct[] = [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2021",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-          },
-          {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2021",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "assets/images/hammer.png"
-          }
+        // {
+        //     "productId": 2,
+        //     "productName": "Garden Cart",
+        //     "productCode": "GDN-0023",
+        //     "releaseDate": "March 18, 2021",
+        //     "description": "15 gallon capacity rolling garden cart",
+        //     "price": 32.99,
+        //     "starRating": 4.2,
+        //     "imageUrl": "assets/images/garden_cart.png"
+        //   },
+        //   {
+        //     "productId": 5,
+        //     "productName": "Hammer",
+        //     "productCode": "TBX-0048",
+        //     "releaseDate": "May 21, 2021",
+        //     "description": "Curved claw steel hammer",
+        //     "price": 8.9,
+        //     "starRating": 4.8,
+        //     "imageUrl": "assets/images/hammer.png"
+        //   }
     ]; //array of any data type
+
+    //note the accessor does not have to be private, remember to import ProductService
+    constructor(private productService : ProductService) {}
 
     //methods listed after properties are defined
 
